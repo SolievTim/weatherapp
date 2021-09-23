7fc17e3ac235b0d4e3aae8e738a7c6743135204d
SolievTim <roman.ushakov1999@mail.ru>
2021-09-23 11:27:26 +0300

    add plusButton+code for

--- SolievWeather/TableViewController.swift
+++ SolievWeather/TableViewController.swift
@@ -24,16 +24,30 @@ class TableViewController: UITableViewController {
     func currentWeather(city: String) {
         let url = "http://api.weatherapi.com/v1/current.json?key=https://github.com/SwiftyJSON/SwiftyJSON.git&q=London&aqi=no\(city)"
         AF.request(url, method: .get).validate().responseJSON { response in switch response.result {
-        case .success(let value):
-            let json = JSON(value)
-            let name = json["location"]["name"].stringValue
-            let temp = json["current"]["temp_c"].doubleValue
-            self.cityTempArray.append(Cities(cityName: name, cityTemp: temp))
-            self.cityTableView.reloadData()
-        case .failure(let error):
-            print(error)
+                case .success(let value):
+                    let json = JSON(value)
+                    let name = json["location"]["name"].stringValue
+                    let temp = json["current"]["temp_c"].doubleValue
+                    self.cityTempArray.append(Cities(cityName: name, cityTemp: temp))
+                    self.cityTableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
         }
+    }
+    @IBAction func addCityAction(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Add", message: "Write a name of city", preferredStyle: .alert)
+        alert.addTextField { (textField) in textField.placeholder = "Moscow"}
+        let cancelAction = UIAlertAction(title: "Close", style: .cancel, handler: nil)
+        let newCityAction = UIAlertAction(title: "Add", style: .default) { (action) in
+            let name = alert.textFields![0].text
+            self.currentWeather(city: name!)
         }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(newCityAction)
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
